174e4976c5328f64709ce00cd3a382ccee8eed15
MatthieuSchmit <matth.sht@gmail.com>
2016-01-19 00:28:15 +0100

    Ajout de gitPush.sh

---
+++ gitPush.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# @author Matthieu - 18 janvier 16
+#
+# gitPush.sh
+
+echo "GitPush"
+echo
+read -p "Message du commit : " message
+echo
+echo "Addresse pour le push ?"
+echo "a. ScriptUtil"
+echo "      (https://github.com/MatthieuSchmit/ScriptUtil.git)"
+echo "b. Arcade"
+echo "      (https://github.com/timchapelle/ARCADE.git)"
+echo "z. Autre"
+read -n 1 adresse
+
+case $adresse in
+	"z"|"Z" )
+		echo "Adresse ?"
+		read adresse
+		;;
+	"a"|"A" )
+		adresse="https://github.com/MatthieuSchmit/ScriptUtil.git"
+		;;
+	"b"|"B" )
+		adresse="https://github.com/timchapelle/ARCADE.git"
+		;;
+	*)
+		echo "Mauvais choix"
+		exit 1
+esac
+
+git add *
+git commit -m "$message"
+git push $adresse
